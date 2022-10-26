00bc6c1f81f0a996fcd14b90d17c713f3f5c3c05
Ulyana.Drozdova <ulyana.drozdova@innowise-group.com>
2022-10-25 17:33:35 -0700

    added html and css file, sketch for input field that will take text to server

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
-from . import views
+from django.urls import re_path
+from . import views 
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    re_path('', views.post_list, name= 'post_list'),
+    re_path(r'^search/', views.search, name= 'search'), 
+    re_path(r'^post_list/', views.post_list, name= 'post_list'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
+from django.utils import timezone
+from .models import Post
+from django.core.exceptions import *
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') #QuerySet
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def search(request):
+    if request.method == 'POST':
+        search_id = request.POST.get('textfield', None)
+    try:
+            user = Person.objects.get(name = search_id)
+            #do something with user
+            html = ("<H1>%s</H1>", user)
+            return HttpResponse(html)
+    except Person.DoesNotExist:
+            return HttpResponse("no such user")  
+    else:
+        return render(request, 'form.html')
