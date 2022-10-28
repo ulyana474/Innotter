1837c5d6b9a64e543f01918c21501ee2dac2d9c9
Ulyana.Drozdova <ulyana.drozdova@innowise-group.com>
2022-10-27 22:39:26 -0700

    add settings to start with postgresql

--- Innotter/settings.py
+++ Innotter/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'Innotter.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db',
+        'USER': 'ulyana474',
+        'PASSWORD': 'psql_ulyana',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
