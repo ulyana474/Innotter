38bd465c39406acf038471bc03d1a404488fea3a
Ulyana.Drozdova <ulyana.drozdova@innowise-group.com>
2023-01-30 17:04:40 -0800

    added username field to posts and pages, rebuilt docker images and pushed to docker hub

--- pages/serializers.py
+++ pages/serializers.py
@@ -2,6 +2,13 @@ from rest_framework import serializers
 from .models import Page
 
 class PageSerializer(serializers.ModelSerializer):
+
+    username = serializers.SerializerMethodField('get_username')
+
     class Meta:
         model = Page
-        fields = ('id', 'name', 'uuid', 'description', 'tags', 'owner', 'followers', 'image', 'is_private', 'follow_requests', 'unblock_date')
+        fields = ('id', 'name', 'uuid', 'description', 'tags', 'owner', 'followers', 'image', 'is_private', 'follow_requests', 'unblock_date', 'username')
+
+    def get_username(self, page):
+        username = page.owner.username
+        return username
--- posts/serializers.py
+++ posts/serializers.py
@@ -2,6 +2,13 @@ from rest_framework import serializers
 from .models import Post
 
 class PostSerializer(serializers.ModelSerializer):
+
+    username = serializers.SerializerMethodField('get_username')
+
     class Meta:
         model = Post
-        fields = ('id', 'page', 'content', 'reply_to', 'likes', 'created_at', 'updated_at')
+        fields = ('id', 'page', 'content', 'reply_to', 'likes', 'created_at', 'updated_at', 'username')
+
+    def get_username(self, post):
+        username = post.page.owner.username
+        return username
